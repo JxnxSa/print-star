c65169c09b78712ec4dbec2112bef1102f01d787
Sxrxwxlxx <tangjittham_s@silpakorn.edu>
2024-03-18 17:43:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Assignment 1
+
+## วิธีการ Run  
+**run บน Terminal** :  
+ 1. run คำสั่ง ```git clone https://github.com/JxnxSa/print-star.git```
+ 2. run คำสั่ง ```npm run dev```
+    
+**run บน web** : 
+ 
+## วิธีการเล่น :
+1. กรอกจำนวนเต็มบวก แล้วกด ```submit```
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [inputValue, setInputValue] = useState(0);
+  const [star, setStar] = useState([]);
+  let arrayStar = [];
+ 
+  function handleSubmit(rows) {
+    for (let i = 1; i <= rows; i++) {
+      let starString = "";
+      for (let j = 1; j <= i; j++) {
+        starString += "*";
+      }
+      arrayStar.push(starString)
+    }
+  
+    for (let i = rows - 1; i > 0; i--) {
+      let starString = "";
+      for (let j = 1; j <= i; j++) {
+        starString += "*";
+      }
+      arrayStar.push(starString) 
+    }
+    setStar(arrayStar)
+  }
+
+  return (
+    <>
+      Input:
+      <input
+        type='number'
+        style={{ margin: 8 }}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={() => handleSubmit(inputValue)}>Submit</button>
+      {star.map(item => (
+        <p>{item}</p>
+      ))}
+    </>
+  );
+}
+
+export default App;
